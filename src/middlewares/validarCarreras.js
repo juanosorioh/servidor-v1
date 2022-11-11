3c91d5bb60044433d96d1b1ed866d13d06ec5308
juanosorioh <juangabrielosorioheredia@gmail.com>
2022-11-11 15:58:51 -0300

    detalle validarCarreras

--- src/middlewares/validarCarreras.js
+++ src/middlewares/validarCarreras.js
@@ -3,8 +3,8 @@ const {check}  = require ('express-validator');
 const validarCarreras  = async (req, res, next) => {
 check('nombre').not().isEmpty().toString(),
 check('descripcion').not().isEmpty().toString(),
-check('profesores').not().isEmpty().toString()
-check('notas').not().isEmpty().toString()
+check('anio').not().isEmpty().toString().isDate(),
+check('materias').not().isEmpty().isArray()
 next();
 } 
     
